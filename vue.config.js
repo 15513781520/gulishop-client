0f22752bcaadd323d468218325a921fea5785de8
tanxingwei <1760842737@qq.com>
2021-04-10 10:52:12 +0800

    初始化提交

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-10 09:56:47
+ * @LastEditTime: 2021-04-10 09:57:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \gulishop-clientt\vue.config.js
+ */
+module.exports = {
+    lintOnSave:false
+}
